583aeead15e7c1f01e30230deff0f1cbfcab81c3
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-23 17:19:40 +0300

    initial commit

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,23 @@
+from secrets import choice
 from django.contrib import admin
 
-from .models import Questions,Choice
+from .models import Questions,Choice,Musician,Album
 
-admin.site.register(Questions)
-admin.site.register(Choice)
+admin.site.site_header="polls Admin"
+admin.site.site_title="polls Admin Area"
+admin.site.index_title="Welcome to polls Admin Area"
+admin.site.register(Musician,Album)
+
+class ChoiceInLine(admin.TabularInline):
+    model=Choice
+    extra=3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldset=[(None, {'fields':['question_text']}),
+    ('Date Information', {'fields':['pub_date'], 'classes': ['collapse']}),]
+    inlines=[ChoiceInLine]
+
+admin.site.register(Questions,QuestionAdmin)
 
 
 # Register your models here.
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Choice, Questions and Choice
+
+def index(request):
+    return render(request, 'poll/sindex.html')
+    
+
 # Create your views here.
