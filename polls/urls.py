6805896fdf23dc20bf73ddc808e34aee80672abe
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-24 17:38:34 +0300

    initial commit

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,8 @@ from polls import views
 
 app_name='polls'
 urlpatterns=[ 
-    path('', views.index, name='index')
-    
-]
+    path('', views.index, name='index'),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
 
+]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,26 @@
+from email.policy import HTTP
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
+from .models import Questions,Choice
+#get questions and display them
 def index(request):
-    return render(request, "polls/index.html")
-    
+    latest_question_list=Questions.objects.order_by('-pub_date')[:5]
+    context={'latest_question_list':latest_question_list}
+    return render(request, "polls/index.html", context)
+
+#show specific question and choices
+def detail(request, question_id):
+    try:
+        question=Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist:
+        raise Http404("Question Does not exist")
+    return render(request,"polls/results.html", {'question': question})
+
+#get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Questions, pk=question_id )
+    return render(request, "polls/results.html", { "question":question})
+
  
     
 
