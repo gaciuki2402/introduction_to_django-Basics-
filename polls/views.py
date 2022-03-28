c5cfede35fded4403fe8b335a6b75cf8207eecfa
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-28 12:40:08 +0300

    initial commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
-from email.policy import HTTP
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Questions,Choice
+
 #get questions and display them
 def index(request):
     latest_question_list=Questions.objects.order_by('-pub_date')[:5]
@@ -21,7 +24,21 @@ def results(request, question_id):
     question = get_object_or_404(Questions, pk=question_id )
     return render(request, "polls/results.html", { "question":question})
 
- 
-    
-
-
+#vote for a question choice
+def vote(request,question_id):
+    #print(request.POST['choice'])
+    question=get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #Always return ana HttResponseRedirect after successfully dealing
+        # with POST data.This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
